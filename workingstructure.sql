149e214c27c5a326ceb7d1a6a7e70e96128e3bc9
Atlantic <bitatlantic@gmail.com>
2015-02-10 20:17:21 +0100

    Update workingstructure.sql

--- workingstructure.sql
+++ workingstructure.sql
@@ -1,1 +1,16 @@
-sql
+USE DB2USE;
+
+DROP TABLE IF EXISTS `packages`;
+CREATE TABLE IF NOT EXISTS `packages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `author` text NOT NULL,
+  `iconurl` text NOT NULL,
+  `version` text NOT NULL,
+  `shortdesc` text NOT NULL,
+  `longdesc` text NOT NULL,
+  `urlto3dsx` text NOT NULL,
+  `urltosmdh` text NOT NULL,
+  `urltocia` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
