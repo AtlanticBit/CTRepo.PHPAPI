d5b53ac6d0999cc9af4fff78d7b49e0f867f99c5
Atlantic <bitatlantic@gmail.com>
2015-02-11 12:35:39 +0100

    yup

--- request.php
+++ request.php
@@ -1,14 +1,6 @@
 <?php
 //server vars
-$debug = false;
-$apiversion = "1.0.0";
-$server = "localhost";
-$user = "3dsclient";
-$password = "test";
-$database = "3dsrepo";
-$repofqdn = "brunoturek.com";
-$reponame = "Bruno Turek's Repo";
-$repodesc = "Best repo in the world!";
+include "config.php";
 //dbconn open
 if($debug) {
 print("1");
