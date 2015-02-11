8a52a92bf0f66c696526c579d2f79a8216128584
Atlantic <bitatlantic@gmail.com>
2015-02-11 22:01:34 +0100

    So yeah

--- structure.sql
+++ structure.sql
@@ -12,8 +12,6 @@ CREATE TABLE IF NOT EXISTS `packages` (
   `version` text NOT NULL,
   `shortdesc` text NOT NULL,
   `longdesc` text NOT NULL,
-  `urlto3dsx` text NOT NULL,
-  `urltosmdh` text NOT NULL,
-  `urltocia` text NOT NULL,
+  `urltopkg` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
